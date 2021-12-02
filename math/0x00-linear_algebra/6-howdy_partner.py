5ce295840d51128c308dbf4b50077930773568df
AndresSern <andrecamposer@gmail.com>
2021-12-01 22:23:03 -0500

    ADding the file 6-main.py and 6-howdy_parter.py with the problem resolved

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Write a function def cat_arrays(arr1, arr2): that concatenates two arrays:
+
+  You can assume that arr1 and arr2 are lists of ints/floats
+  You must return a new list
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """ Function that concatenates two arrays """
+    return arr1 + arr2
