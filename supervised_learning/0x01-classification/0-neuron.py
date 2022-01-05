71e22968b0b404f1f7f03a69f5a8280adb69b20e
AndresSern <andrecamposer@gmail.com>
2022-01-04 23:46:19 -0500

    Fixing

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -15,6 +15,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.W = np.ndarray((1, nx))
-        self.W = np.random.normal(size=nx)
+        self.W = np.random.normal(self.W[0], size=nx)
         self.b = 0
         self.A = 0
