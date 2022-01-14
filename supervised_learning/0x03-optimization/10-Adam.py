93ad61305787517df6fd666b713bd4089b89b529
AndresSern <andrecamposer@gmail.com>
2022-01-13 23:24:19 -0500

    Adding 10-adam.py

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
-"""Fix comment"""
+"""
+Function that creates the training operation
+for a neural network in tensorflow using the
+Adam optimization algorithm
+"""
 
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
-    """Fix comment"""
-    return None
+    """
+    Returns: the Adam optimization operation
+    """
+    adam_optimizer = tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon)
+    trainer = adam_optimizer.minimize(loss)
+    return trainer
